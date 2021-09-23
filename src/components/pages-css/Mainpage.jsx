d07634c24c7823be9e22a0ff32b0bd325a08f803
olanrewajusec <oa211048@gmail.com>
2021-09-23 12:03:12 +0100

    form fields now clear after submission

--- src/components/pages-css/Mainpage.jsx
+++ src/components/pages-css/Mainpage.jsx
@@ -1,17 +1,23 @@
 import styled from "@emotion/styled";
 
 export const MainpageStyle = styled.div`
-    border: 2px solid #d3d3d3;
-    border-radius: .5em;
-    margin-bottom: 0em;
-    margin-left: auto;
-    margin-right: auto;
-    padding: 1em;
-    text-align: left;
-    width: 600px;
-    margin-top: 25%;
+    height: 100vh;
+    width: auto;
+    padding-top: 27vh;
 `
 
 export const FormStyle = styled.div`
+    text-align: left;
+    width: 600px;
+    padding: 1em;
+    border: 2px solid #d3d3d3;
+    border-radius: .5em;
+    margin-top: auto;
+    margin-bottom: auto;
+    margin-left: auto;
+    margin-right: auto;
+`
+
+export const InnerFormStyle = styled.div`
     padding: 1em;
 `
--- src/components/pages/Mainpage.jsx
+++ src/components/pages/Mainpage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Row, Col, Form, FormGroup, Input, Button, FormFeedback } from 'reactstrap';
-import { MainpageStyle, FormStyle } from '../pages-css/Mainpage';
+import { MainpageStyle, FormStyle, InnerFormStyle } from '../pages-css/Mainpage';
 import axios from "axios";
 
 const Mainpage = () => {
@@ -18,23 +18,8 @@ const Mainpage = () => {
     const onInputChange = e => {
         const { name, value } = e.target;
         setCredential({ ...credential, [name]: value });
-        if (e.target.type === "email") validateEmail(e)
-    };
-
-    const onSubmit = event => {
-        event.preventDefault();
-        const { fullname, email, comment } = credential;
-
-        axios.post('http://localhost:8000/contact/', { fullname, email, comment })
-            .catch((err) => {
-                if (err.response) {
-                    console.log(err.response.data);
-                    console.log(err.response.status);
-                    console.log(err.response.headers);
-                } else if (err.request) {
-                    console.log(err.request);
-                } else console.log('Error', err.message);
-            })
+        console.log(credential);
+        if (e.target.type === "email") validateEmail(e);
     };
 
     const validateEmail = (e) => {
@@ -45,57 +30,81 @@ const Mainpage = () => {
         } else setEmailState('has-danger');
     }
 
+    const onSubmit = event => {
+        event.preventDefault();
+        const { fullname, email, comment } = credential;
+        
+        axios.post('http://localhost:8000/contact/', { fullname, email, comment })
+            .catch((err) => {
+                if (err.response) {
+                    console.log(err.response.data);
+                    console.log(err.response.status);
+                    console.log(err.response.headers);
+                } else if (err.request) {
+                    console.log(err.request);
+                } else console.log('Error', err.message);
+            });
+        setEmailState("");
+        setCredential(clientCredential);
+    };
+
     return (
         <MainpageStyle>
-            <Form>
-                <FormStyle>
-                    <FormGroup>
-                        <Input
-                            type="textarea"
-                            name="comment"
-                            placeholder="Hello! what will you like me to do for you? 😇"
-                            onChange={onInputChange}
-                            style={{height: 300}}
-                        />
-                    </FormGroup>
-
-                    <Row form>
-                        <Col md={6}>
-                            <FormGroup>
-                                <Input
-                                    type="fullname" name="fullname" placeholder="please enter your fullname"
-                                    onChange={onInputChange}
-                                />
-                            </FormGroup>
-                        </Col>
-
-                        <Col md={6}>
-                            <FormGroup>
-                                <Input
-                                    id="email"
-                                    type="email"
-                                    name="email" placeholder="please enter your email"
-                                    onChange={onInputChange}
-                                    valid={emailState === 'has-success'}
-                                    invalid={emailState === 'has-danger'}
-                                />
-                                <FormFeedback >
-                                    Looks like there is an issue with your email. Please input a correct email.
-                                </FormFeedback>
-                            </FormGroup>
-                        </Col>
-                    </Row>
-                </FormStyle>
-
-                <Button
-                    block
-                    onClick={onSubmit}
-                    color="primary"
-                    size="md"
+            <FormStyle>
+                <Form
+                    onSubmit={onSubmit}
                 >
-                    Submit
-                </Button>
-            </Form>
+                    <InnerFormStyle>
+                        <FormGroup>
+                            <Input
+                                type="textarea"
+                                name="comment"
+                                placeholder="Hello! what will you like me to do for you? 😇"
+                                onChange={onInputChange}
+                                style={{ height: 300 }}
+                                value={credential.comment}
+                            />
+                        </FormGroup>
+
+                        <Row form>
+                            <Col md={6}>
+                                <FormGroup>
+                                    <Input
+                                        type="fullname" name="fullname" placeholder="please enter your fullname"
+                                        onChange={onInputChange}
+                                        value={credential.fullname}
+                                    />
+                                </FormGroup>
+                            </Col>
+
+                            <Col md={6}>
+                                <FormGroup>
+                                    <Input
+                                        type="email"
+                                        name="email" placeholder="please enter your email"
+                                        onChange={onInputChange}
+                                        valid={emailState === 'has-success'}
+                                        invalid={emailState === 'has-danger'}
+                                        value={credential.email}
+                                    />
+                                    <FormFeedback >
+                                        Looks like there is an issue with your email. Please input a correct email.
+                                    </FormFeedback>
+                                </FormGroup>
+                            </Col>
+                        </Row>
+
+                        <Button
+                            block
+                            type="submit"
+                            color="primary"
+                            size="md"
+                        >
+                        Submit
+                    </Button>
+                    </InnerFormStyle>
+                </Form>
+            </FormStyle>
         </MainpageStyle>
     )
 }
